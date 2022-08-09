9f0f969063fd80f7ad8d9a6983bb43b66ab7d869
Vinicius <100228873+Rockyiu@users.noreply.github.com>
2022-08-09 20:30:35 -0300

    Add files via upload

---
+++ solucaoboa/src/main/java/application/Program.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package application;
+
+import entities.Reservation;
+import exceptions.DomainException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        
+        try{
+            System.out.print("Room number:");
+            int roomNumber = sc.nextInt();
+            System.out.print("Check-in date (dd/MM/yyyy)");
+            Date checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy)");
+            Date checkOut = sdf.parse(sc.next());
+
+             Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+             System.out.println("Reservation:" + reservation);
+
+            System.out.println();
+            System.out.println("Enter data to update the reservation:");
+            System.out.print("Check-in date (dd/MM/yyyy)");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy)");
+            checkOut = sdf.parse(sc.next());
+
+            reservation.updateDates(checkIn, checkOut);
+            System.out.println("Reservation:" + reservation);
+        }
+        catch (ParseException e){
+            System.out.println("Invalid Date");
+        }
+        catch(DomainException e){
+            System.out.println("Error in reservation:" + e.getMessage());
+        }
+        catch(RuntimeException e){
+            System.out.println("Unexpected error");
+        }
+    }
+    
+}
